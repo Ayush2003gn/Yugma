88dee22537631b6a69155270c9d4f0325f4e9b0d
Ayush2003gn <198405311+Ayush2003gn@users.noreply.github.com>
2026-05-13 12:58:51 +0530

    fixed bug

--- core/models/tasklist.py
+++ core/models/tasklist.py
@@ -16,9 +16,10 @@ class TaskList:
 
     #----------------------------------Create task---------------------------------------------
     def add_task(self,task):
-        id = str(uuid.uuid4())
+        internal_id = str(uuid.uuid4())
         ui_id = self.ui_id_generator()
-        newtask = Task(task, id, ui_id, datetime_now(), datetime_now())
+
+        newtask = Task(task, internal_id, ui_id, datetime_now(), datetime_now())
         
         self.tasklist.append(newtask)
         self.changed = True
@@ -26,113 +27,112 @@ class TaskList:
         logger.info(f"{self.category} page:task added [{task}]")
         return {"success": True,"message": f"{self.category} page:task added [{newtask.task}]","task": newtask}
     
-    def importing_task(self,task, id, created_date, modified_date, priority, done):
+    def importing_task(self,task, internal_id, created_date, modified_date, priority, done):
         ui_id = self.ui_id_generator()
-        newtask = Task(task, id, ui_id, datetime.datetime.fromisoformat(created_date), datetime.datetime.fromisoformat(modified_date), priority, done)
+        newtask = Task(task, internal_id, ui_id, datetime.datetime.fromisoformat(created_date), datetime.datetime.fromisoformat(modified_date), priority, done)
         self.tasklist.append(newtask)
 
     def ui_id_generator(self):
         self.ui_id_count += 1
         return self.ui_id_count
     
-    def id_find(self,id):
+    def internal_id_find(self,internal_id):
         for task in self.tasklist:
-            if task.id == id:
+            if task.internal_id == internal_id:
                 return task
             
         return None
     
     #----------------------------------Remove task---------------------------------------------
-    def remove_task(self, id):
-        task = self.id_find(id)
+    def remove_task(self, internal_id):
+        task = self.id_find(internal_id)
         if task:
             self.tasklist.remove(task)
-            logger.info(f"{self.category} page: Id : {id} is removed")
+            logger.info(f"{self.category} page: Id : {internal_id} is removed")
             self.changed = True
-            return {"success": True,"message":f"Id : {id} is removed"}
+            return {"success": True,"message":f"Id : {internal_id} is removed"}
         
-        logger.warning(f"{self.category} page: Task ID not found: {id} ")
+        logger.warning(f"{self.category} page: Task ID not found: {internal_id} ")
         return {"success": False,"message":f"Id not found in {self.category} page"}
     
     #----------------------------------Update task---------------------------------------------    
-    def update_task(self,id,task):
-        task_found = self.id_find(id)
+    def update_task(self, internal_id, task):
+        task_found = self.id_find(internal_id)
         if task_found == None:
-            logger.warning(f"{self.category} page: Task ID not found: {id}")
+            logger.warning(f"{self.category} page: Task ID not found: {internal_id}")
             return {"success": False,"message":f"Id not found in {self.category} page" }
         
         else:
             task_found.correction(task,datetime_now())
-            logger.info(f"{self.category} page: Task updated: id={id}, new_value='{task}'")
+            logger.info(f"{self.category} page: Task updated: id={internal_id}, new_value='{task}'")
             self.changed = True
-            return {"success": True,"message":f"successfully Update task of id {id} in {self.category} page","task":task_found}
+            return {"success": True,"message":f"successfully Update task of id {internal_id} in {self.category} page","task":task_found}
     
-    def mark_done(self,id):
-        task_found = self.id_find(id)
+    def mark_done(self, internal_id):
+        task_found = self.id_find(internal_id)
         if task_found == None:
-            logger.warning(f"{self.category} page: Task ID not found: {id} ")
+            logger.warning(f"{self.category} page: Task ID not found: {internal_id} ")
             return {"success": False,"message":f"Id not found in {self.category} page"}
         else:
             task_found.mark_done(datetime_now())
-            logger.info(f"{self.category} page: Task status updated: {id} ")
+            logger.info(f"{self.category} page: Task status updated: {internal_id} ")
             self.changed = True
-            return {"success": True,"message":f"successfully Update status of id {id} in {self.category} page","task":task_found}
+            return {"success": True,"message":f"successfully Update status of id {internal_id} in {self.category} page","task":task_found}
         
-    def mark_undone(self,id):
-        task_found = self.id_find(id)
+    def mark_undone(self,internal_id):
+        task_found = self.id_find(internal_id)
         if task_found == None:
-            logger.warning(f"{self.category} page: Task ID not found: {id} ")
+            logger.warning(f"{self.category} page: Task ID not found: {internal_id} ")
             return {"success": False,"message":f"Id not found in {self.category} page"}
         else:
             task_found.mark_undone(datetime_now())
-            logger.info(f"{self.category} page: Task status updated: {id}")
+            logger.info(f"{self.category} page: Task status updated: {internal_id}")
             self.changed = True
-            return {"success": True,"message":f"successfully Update status of id {id} in {self.category} page","task":task_found}
+            return {"success": True,"message":f"successfully Update status of id {internal_id} in {self.category} page","task":task_found}
     
-    def highpriority_task(self,id):
-        task_found = self.id_find(id)
+    def high_priority_task(self,internal_id):
+        task_found = self.id_find(internal_id)
         if task_found == None:
-            logger.warning(f"{self.category} page: Task ID not found: {id} ")
+            logger.warning(f"{self.category} page: Task ID not found: {internal_id} ")
             return {"success": False,"message":f"Id not found in {self.category} page"}
         else:
             task_found.priority_high(datetime_now())
-            logger.info(f"{self.category} page: Task priority level updated: {id} ")
+            logger.info(f"{self.category} page: Task priority level updated: {internal_id} ")
             self.changed = True
-            return {"success": True,"message":f"successfully Update priority level of id {id} in {self.category} page","task":task_found}
+            return {"success": True,"message":f"successfully Update priority level of id {internal_id} in {self.category} page","task":task_found}
         
-    def Normalpriority_task(self,id):
-        task_found = self.id_find(id)
+    def Normal_priority_task(self,internal_id):
+        task_found = self.id_find(internal_id)
         if task_found == None:
-            logger.warning(f"{self.category} page: Task ID not found: {id} ")
+            logger.warning(f"{self.category} page: Task ID not found: {internal_id} ")
             return {"success": False,"message":"Id not found"}
         else:
             task_found.priority_normal(datetime_now())
-            logger.info(f"{self.category} page: Task priority level updated: {id}")
+            logger.info(f"{self.category} page: Task priority level updated: {internal_id}")
             self.changed = True
-            return {"success": True,"message":f"successfully Update priority level of id {id} in {self.category} page","task":task_found}
+            return {"success": True,"message":f"successfully Update priority level of id {internal_id} in {self.category} page","task":task_found}
         
-    def lowpriority_task(self,id):
-        task_found = self.id_find(id)
+    def low_priority_task(self,internal_id):
+        task_found = self.id_find(internal_id)
         if task_found == None:
-            logger.warning(f"{self.category} page: Task ID not found: {id} ")
+            logger.warning(f"{self.category} page: Task ID not found: {internal_id} ")
             return {"success": False,"message":"Id not found"}
         else:
             task_found.priority_low(datetime_now())
-            logger.info(f"{self.category} page: Task priority level updated: {id} ")
+            logger.info(f"{self.category} page: Task priority level updated: {internal_id} ")
             self.changed = True
-            return {"success": True,"message":f"successfully Update priority level of id {id} in {self.category} page","task":task_found}
+            return {"success": True,"message":f"successfully Update priority level of id {internal_id} in {self.category} page","task":task_found}
     
     #----------------------------------Display task---------------------------------------------
     def display_all(self):
         print()
-        print("# ",self.category)
         if not self.tasklist:
-            print("No tasks available")
             logger.warning("{self.category} page: No tasks available ")
-            return
-        for task in self.tasklist:
-            print(task)
+            return "No tasks available"
         logger.debug(f"{self.category} page: Displayed {len(self.tasklist)}/{len(self.tasklist)} tasks ")
+        return self.tasklist
+
+
 
     def display_bymonths(self,months,year):
         print()
