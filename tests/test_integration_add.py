702416ccec676926a4048993074cd9ab9c33334a
Ayush2003gn <198405311+Ayush2003gn@users.noreply.github.com>
2026-05-23 07:52:19 +0530

    Stabilize architecture before merge

--- tests/test_integration_add.py
+++ tests/test_integration_add.py
@@ -3,10 +3,10 @@ from core import action
 
 def test_add_task_flow():
 
-    parsed = token.parse_command(
+    cmd,arg = token.parse_command(
         'add -t "Learn Python"'
     )
-
+    parsed = token.command_handler(cmd, arg)
     result = action.execute_command(parsed)
 
     assert result["success"] is True
