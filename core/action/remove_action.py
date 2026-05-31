70b49ecbba4fa39f7839bd2422fce0d34ddeb256
Ayush2003gn <198405311+Ayush2003gn@users.noreply.github.com>
2026-05-31 08:22:36 +0530

    feat: add remove_action

--- core/action/remove_action.py
+++ core/action/remove_action.py
@@ -4,4 +4,38 @@ from core.contracts.error_data import ErrorData
 logger = logging.getLogger(__name__)
 
 def remove_action(token_return,task_app):
-    pass
+    if not token_return:
+        logger.error("token_return was None")
+        return OperationResult(
+            success=False,
+            error=ErrorData(
+                error_boolean=True,
+                error_message="something went wrong",
+                error_code="error-no-return-value-from-token"
+            )
+        )
+    
+    if token_return.action == "remove-task":
+        if token_return.task_uid is not None:
+            logger.info(f"Removing task with uid {token_return.task_uid}")
+            iid_reture = task_app.resolve_uid(token_return.task_uid,token_return.page_name)
+            if iid_reture.success is False:
+
+                logger.warning(iid_reture.message)
+                return iid_reture
+            
+            remove_task = task_app.remove_task(iid_reture.data,token_return.page_name)
+            
+            if remove_task.success is False:
+
+                logger.warning(remove_task.message)
+                return OperationResult(
+                    success=False,
+                    error=ErrorData(
+                        error_boolean=True,
+                        error_message=remove_task.error.error_message,
+                        error_code=remove_task.error.error_code
+                    )
+                )
+
+            return remove_task
