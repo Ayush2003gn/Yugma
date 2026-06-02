6fbf8d51989d676c04b892eacca1f01af314327f
Ayush2003gn <198405311+Ayush2003gn@users.noreply.github.com>
2026-06-02 05:52:17 +0530

    fix error and add logs

--- core/action/action_manager.py
+++ core/action/action_manager.py
@@ -25,26 +25,55 @@ def command_paths():
 
 def decision_action(token_return):
     command = token_return.command
-    
+    logger.info(f"command: {command}")
     if command == "page":
+        logger.info("page command")
         return page_action.page_action(token_return,task_app)
     
     elif command == "add":
+        logger.info("add command")
         return add_action.add_action(token_return,task_app)
     
     elif command == "update":
+        logger.info("update command")
         return update_action.update_action(token_return,task_app)
     
     elif command == "remove":
+        logger.info("remove command")
         return remove_action.remove_action(token_return,task_app)
+    
     elif command == "priority":
+        logger.info("priority command")
         return priority_action.priority_action(token_return,task_app)
+    
     elif command =="mark_done":
+        logger.info("mark_done command")
         return mark_done_action.mark_done_action(token_return,task_app)
+    
     elif command == "mark_undone":
+        logger.info("mark_undone command")
         return mark_undone_action.mark_undone_action(token_return,task_app)
+    
     elif command == "display":
+        logger.info("display command")
         return display_action.display_action(token_return,task_app)
+    
+    elif command == "help":
+        logger.info("help command")
+        return display_action.cmd_exit()
+    
+    elif command == "exit":
+        logger.info("exit command")
+        return display_action.cmd_exit()
+    
+    elif command == None:
+        logger.warning("No command provided by user")
+        return display_action.cmd_none()
+    
+    else:
+        logger.warning("Invalid command provided by user")
+        return display_action.cmd_invalid(token_return)
 
 def action_manager(token_return):
-    return storage_action.storage_action(token_return,task_app)
+    #return storage_action.storage_action(token_return,task_app)
+    pass
