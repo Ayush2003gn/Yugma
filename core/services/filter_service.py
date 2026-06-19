14c5ab089df482d7a54f0b9ddf740ddbc6501c49
Ayush2003gn <198405311+Ayush2003gn@users.noreply.github.com>
2026-06-19 06:56:00 +0530

    feat(filter): enhance filter method with comprehensive task filtering options

--- core/services/filter_service.py
+++ core/services/filter_service.py
@@ -4,7 +4,37 @@ class FilterService:
         self.taskapp = taskapp
     
     def filter(self,task=None,done=None,priority=None,tag=None,page_id=None,group=None,iid=None,uid=None,date_created=None,date_modified=None):
-        if type(self.taskapp) != classmethod:
-            raise TypeError("taskapp must be an instance of TaskApp class")
-        
+        all_tasks = self.taskapp.get_all_tasks()
+        filtered_tasks = all_tasks
+        if task is not None:
+            filtered_tasks = [t for t in filtered_tasks if t.task == task]
+        if done is not None:
+            filtered_tasks = [t for t in filtered_tasks if t.done == done]
+        if priority is not None:
+            filtered_tasks = [t for t in filtered_tasks if t.priority == priority]
+        if tag is not None:
+            tag = tag.lower()
+            tag_filtered_tasks = []
+            if isinstance(tag, list):
+                for ftag in tag:
+                    for task in filtered_tasks:
+                        if ftag == task.tag.lower():
+                            tag_filtered_tasks.append(task)
+                filtered_tasks = tag_filtered_tasks
+   
+        if page_id is not None:
+            filtered_tasks = [t for t in filtered_tasks if t.page_id == page_id]
+        if iid is not None:
+            filtered_tasks = [t for t in filtered_tasks if t.iid == iid]
+        if uid is not None:
+            filtered_tasks = [t for t in filtered_tasks if t.uid == uid]
+        if date_created is not None:
+            filtered_tasks = [t for t in filtered_tasks if t.date_created == date_created]
+        if date_modified is not None:
+            filtered_tasks = [t for t in filtered_tasks if t.date_modified == date_modified]
+        return FilterReturn(
+            success = True,
+            message = "filtering completed",
+            data = filtered_tasks
+        )
         
